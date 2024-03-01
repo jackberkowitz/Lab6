5e4cfd161865f1bad061da0f44677d73fda76986
jackberkowitz <106778796+jackberkowitz@users.noreply.github.com>
2024-03-01 10:50:55 -0500

    Add files via upload

--- Lab6.c
+++ Lab6.c
@@ -34,7 +34,7 @@ int main(void)
 				//we can enable ADC to trigger interrupt
 		
 	//Set Up Motor Control			
-	OCr0A = 0x00; //Load $00 into OCR0 to set initial duty cycle to 0 (motor off)
+	OCR0A = 0x00; //Load $00 into OCR0 to set initial duty cycle to 0 (motor off)
 	TCCR0A = 0b10000011; //Set non-inverting mode on OC0A pin (COMA1:0 = 10)
 						 //Fast PWM (WGM1:0 bits = bits 1:0 = 11)
 						 //(Note: that we are not affecting OC0B because COMB0:1 bits
@@ -73,7 +73,7 @@ int main(void)
 			PORTB = 0b00000010; //motor reversed
 			OCR0A = ((pvalue*2)-255)*-1;
 		}
-		else if(pvalue = 128) //middle voltage range (exactly 0% duty cycle)
+		else if(pvalue == 128) //middle voltage range (exactly 0% duty cycle)
 		{
 			PORTC = 0b11111011; //turn LED 2 (PC2) on, rest off
 			PORTB = 0b00000000; //motor stopped
@@ -85,7 +85,6 @@ int main(void)
 			PORTB = 0b00000100; //motor forward
 			OCR0A = (pvalue*2)-255;
 		}
-		
 		else if(pvalue < 204) //2nd highest voltage range (20% to 60% duty cycle)
 		{
 			PORTC = 0b11110111; //turn LED 3 (PC3) on, rest off 
